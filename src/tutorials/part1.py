eabd6df42fd1c7816bc4e8d21e68e844c5c32413
jpm16011 <janusmanlapaz@gmail.com>
2018-08-03 16:47:38 -0400

    Project setup, first tutorial completed

---
+++ src/tutorials/part1.py
@@ -0,0 +1,15 @@
+""" Creates a simple bot that creates a game instance and distributes workers"""
+import sc2
+from sc2 import run_game, maps, Race, Difficulty
+from sc2.player import Bot, Computer
+
+
+class JanusBot(sc2.BotAI):
+    async def on_step(self, iteration):
+        await self.distribute_workers()
+
+
+run_game(maps.get("AbyssalReefLE"), [
+    Bot(Race.Protoss, JanusBot()),
+    Computer(Race.Terran, Difficulty.Easy)
+], realtime=True)
